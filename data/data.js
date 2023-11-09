ab1cddb32feff4c3f3dfae6aec256ee9c676959e
ilyatitovich <ilyatitovich@gmail.com>
2023-11-09 11:54:31 +0100

    portfolio project, smooth scroll

--- data/data.js
+++ data/data.js
@@ -2,13 +2,23 @@ export const projectsList = {
     portfolio: {
         id: 1,
         title: "Portfolio 2023",
-        cover: "/projects/cover-2.png",
+        cover: "/projects/portfolio/portfolio-cover.webp",
         category: "Web Development",
-        description: "Personal web site",
-        techStack: "React/Next.js, Framer Motion",
+        description: "The primary goal of this project was to create a digital portfolio that serves as a repository for information about my skills, showcasing a diverse array of projects,  and offering essential contact information.",
+        techStack: "React/Next.js, Framer Motion, Lenis",
         path:"/projects/portfolio",
         website: "https://ilyatitovich.vercel.app",
-        screenShots: [],
+        screenShots: [
+            "/projects/portfolio/sreenshots/01-home-light.webp",
+            "/projects/portfolio/sreenshots/02-home-dark.webp",
+            "/projects/portfolio/sreenshots/03-about-light.webp",
+            "/projects/portfolio/sreenshots/04-about-dark.webp",
+            "/projects/portfolio/sreenshots/05-menu-light.webp",
+            "/projects/portfolio/sreenshots/06-menu-dark.webp",
+            "/projects/portfolio/sreenshots/07-contact-light.webp",
+            "/projects/portfolio/sreenshots/08-contact-dark.webp",
+            "/projects/portfolio/sreenshots/09-mobile-light.webp"
+        ],
     },
 
     chrispratt: {
--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -8,18 +8,22 @@ import { projectsList } from "@/data/data";
 import { motion } from "framer-motion";
 import { pageItem } from "@/components/animation";
 import { MenuContext } from "@/components/MenuProvider";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Project() {
     const { menuIsOpen } = useContext(MenuContext);
+
     const router = useRouter();
+    const { project } = router.query;
+
     const [currentProject, setCurrentProject] = useState("");
     const [hovered, setHovered] = useState(false);
 
     useEffect(() => {
         if (router.isReady) {
-            setCurrentProject(projectsList[router.query.project]);
+            setCurrentProject(projectsList[project]);
         }
-    }, [router.query.project]);
+    }, [project]);
 
     const {
         title,
@@ -32,165 +36,181 @@ export default function Project() {
         nextProject,
     } = currentProject || {};
 
+    const screenShotItems =
+        currentProject &&
+        screenShots.map((screenShot) => {
+            return (
+                <div key={screenShot} className="project-content-wrapper">
+                    <Image
+                        src={screenShot}
+                        alt={screenShot.split("/").at(-1)}
+                        width={2000}
+                        height={1070}
+                        className="project-content-image"
+                    />
+                </div>
+            );
+        });
+
     return currentProject ? (
         <>
             <Head>
                 <title>Ilya Titov | {title}</title>
             </Head>
-            <div className="page-content">
-                <div className="project">
-                    <motion.section
-                        variants={pageItem}
-                        custom={2}
-                        initial="hide"
-                        animate={
-                            menuIsOpen || !router.isReady ? "hide" : "show"
-                        }
-                        exit="hide"
-                        className="header-project"
-                    >
-                        <div className="hero-image-wrapper">
-                            <Image
-                                src={cover}
-                                fill={true}
-                                sizes="100vw"
-                                alt="project cover"
-                                className="hero-image"
-                            />
-                        </div>
-                    </motion.section>
-
-                    <motion.section
-                        variants={pageItem}
-                        custom={0}
-                        initial="hide"
-                        animate={menuIsOpen ? "hide" : "show"}
-                        exit="hide"
-                        className="project-title"
-                    >
-                        <div className="page-head">
-                            <h2 className="page-title">{title}</h2>
-                        </div>
-                        <hr className="head-separator" />
-                    </motion.section>
-
-                    <motion.section
-                        variants={pageItem}
-                        custom={2}
-                        initial="hide"
-                        animate={menuIsOpen ? "hide" : "show"}
-                        exit="hide"
-                        className="project-intro"
-                    >
-                        <div className="project-data">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <h6>Category</h6>
-                                        </td>
-                                        <td>
-                                            <ul>
-                                                <li>{category}</li>
-                                            </ul>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <h6>Tech Stack</h6>
-                                        </td>
-                                        <td>
-                                            <p>{techStack}</p>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="project-description">
-                            <p>{description}</p>
-                            <div className="project-website">
-                                <div className="link-wrapper">
-                                    <div className="link">
-                                        <Link
-                                            href={website}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            ↗ View the website
-                                        </Link>
-                                    </div>
-                                    <div className="link-underline" />
-                                </div>
+            <ReactLenis root>
+                <div className="page-content">
+                    <div className="project">
+                        <motion.section
+                            variants={pageItem}
+                            custom={2}
+                            initial="hide"
+                            animate={menuIsOpen ? "hide" : "show"}
+                            exit="hide"
+                            className="header-project"
+                        >
+                            <div className="hero-image-wrapper">
+                                <Image
+                                    src={cover}
+                                    fill={true}
+                                    sizes="100vw"
+                                    alt="project cover"
+                                    className="hero-image"
+                                />
                             </div>
-                        </div>
-                    </motion.section>
+                        </motion.section>
 
-                    <motion.section
-                        variants={pageItem}
-                        custom={1}
-                        initial="hide"
-                        animate={menuIsOpen ? "hide" : "show"}
-                        exit="hide"
-                        className="project-content"
-                    >
-                        <h6>{title}</h6>
-                        <div className="project-content-wrapper">
-                            {/* project images */}
-                        </div>
-                    </motion.section>
+                        <motion.section
+                            variants={pageItem}
+                            custom={0}
+                            initial="hide"
+                            animate={menuIsOpen ? "hide" : "show"}
+                            exit="hide"
+                            className="project-title"
+                        >
+                            <div className="page-head">
+                                <h2 className="page-title">{title}</h2>
+                            </div>
+                            <hr className="head-separator" />
+                        </motion.section>
 
-                    <motion.div
-                        variants={pageItem}
-                        custom={0}
-                        initial="hide"
-                        animate={menuIsOpen ? "hide" : "show"}
-                        exit="hide"
-                        className="next-project"
-                        onMouseEnter={() => setHovered(true)}
-                        onMouseLeave={() => setHovered(false)}
-                    >
-                        <hr />
-                        <Link href={nextProject.path}>
-                            <div className="next-project-wrapper">
-                                <div className="next-project-left">
-                                    <div className="next-project-selected-wrapper">
+                        <motion.section
+                            variants={pageItem}
+                            custom={2}
+                            initial="hide"
+                            animate={menuIsOpen ? "hide" : "show"}
+                            exit="hide"
+                            className="project-intro"
+                        >
+                            <div className="project-data">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <h6>Category</h6>
+                                            </td>
+                                            <td>
+                                                <ul>
+                                                    <li>{category}</li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h6>Tech Stack</h6>
+                                            </td>
+                                            <td>
+                                                <p>{techStack}</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="project-description">
+                                <p>{description}</p>
+                                {project !== "portfolio" && (
+                                    <div className="project-website">
+                                        <div className="link-wrapper">
+                                            <div className="link">
+                                                <Link
+                                                    href={website}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    ↗ View the website
+                                                </Link>
+                                            </div>
+                                            <div className="link-underline" />
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </motion.section>
+
+                        <motion.section
+                            variants={pageItem}
+                            custom={1}
+                            initial="hide"
+                            animate={menuIsOpen ? "hide" : "show"}
+                            exit="hide"
+                            className="project-content"
+                        >
+                            <h6>Screenshots</h6>
+                            {screenShotItems}
+                        </motion.section>
+
+                        <motion.div
+                            variants={pageItem}
+                            custom={0}
+                            initial="hide"
+                            animate={menuIsOpen ? "hide" : "show"}
+                            exit="hide"
+                            className="next-project"
+                            onMouseEnter={() => setHovered(true)}
+                            onMouseLeave={() => setHovered(false)}
+                        >
+                            <hr />
+                            <Link href={nextProject.path}>
+                                <div className="next-project-wrapper">
+                                    <div className="next-project-left">
+                                        <div className="next-project-selected-wrapper">
+                                            <motion.h4
+                                                initial={{ x: "-100%" }}
+                                                animate={{
+                                                    x: hovered ? 0 : "-100%",
+                                                }}
+                                                transition={{
+                                                    type: "tween",
+                                                }}
+                                                className="next-project-selected"
+                                            >
+                                                →
+                                            </motion.h4>
+                                        </div>
                                         <motion.h4
-                                            initial={{ x: "-100%" }}
+                                            initial={{ x: 0 }}
                                             animate={{
-                                                x: hovered ? 0 : "-100%",
+                                                x: hovered ? 24 : 0,
                                             }}
                                             transition={{
                                                 type: "tween",
                                             }}
-                                            className="next-project-selected"
+                                            className="next-project-title"
                                         >
-                                            →
+                                            Next project
                                         </motion.h4>
                                     </div>
-                                    <motion.h4
-                                        initial={{ x: 0 }}
-                                        animate={{
-                                            x: hovered ? 24 : 0,
-                                        }}
-                                        transition={{
-                                            type: "tween",
-                                        }}
-                                        className="next-project-title"
-                                    >
-                                        Next project
-                                    </motion.h4>
+                                    <div className="next-project-right">
+                                        <h4>{nextProject.title}</h4>
+                                        <p>{nextProject.category}</p>
+                                    </div>
                                 </div>
-                                <div className="next-project-right">
-                                    <h4>{nextProject.title}</h4>
-                                    <p>{nextProject.category}</p>
-                                </div>
-                            </div>
-                        </Link>
-                    </motion.div>
+                            </Link>
+                        </motion.div>
+                    </div>
                 </div>
-            </div>
+            </ReactLenis>
         </>
     ) : (
-        router.query.project && <Custom404/>
+        project && <Custom404 />
     );
 }
